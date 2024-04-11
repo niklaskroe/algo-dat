75c125161c224f226ac72229be0ec7b66d5a8bca
Niklas Kröger <niklas.kroeger@dm.de>
2024-04-11 14:16:13 +0200

    add blatt05 with aufgabe12 to aufgabe15

---
+++ src/SearchAndSort/Comparator.java
@@ -0,0 +1,12 @@
+package SearchAndSort;
+
+import java.util.List;
+
+public class Comparator<T> {
+    public T[] list;
+    public Comparator(int len) {
+        list = (T[]) new Object[len];
+
+    }
+
+}
